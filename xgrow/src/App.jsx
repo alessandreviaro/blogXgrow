055f31bfe2f69ff74da7ac7b19ea0ca6c0557177
Alessandre <156126428+alessandreviaro@users.noreply.github.com>
2024-04-09 11:37:54 -0300

    adding hooks for our login, register code

--- xgrow/src/App.jsx
+++ xgrow/src/App.jsx
@@ -1,13 +1,15 @@
 import Routes from './Routes/routes';
 import './App.css'
+import { AuthProvider } from './Contexts/Auth';
 
 
 function App() {
   return (
-    
+    <AuthProvider>
     <div> 
       <Routes />
     </div>
+    </AuthProvider>
     
   );
 }
