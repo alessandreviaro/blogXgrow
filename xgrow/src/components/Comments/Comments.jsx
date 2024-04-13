e5c3e4970b4c4436afcfdf0db7561d346df9600e
Alessandre <156126428+alessandreviaro@users.noreply.github.com>
2024-04-13 13:52:43 -0300

    inserting fetch in comments page

--- xgrow/src/components/Comments/Comments.jsx
+++ xgrow/src/components/Comments/Comments.jsx
@@ -1,12 +1,18 @@
 import './style.css'
 
-const Comment = () => {
+
+
+const Comment = ({items}) => {
+
+    const initialPost = items.find(item => item.id === '1');
+
     return (
         <div className='comments'>
+            <div key={initialPost.id} ></div>
             <img className='image' src='https://source.unsplash.com/random/40x40?sig=1' alt='random'></img>
             <ul>
-                <li><p className='user-name'><b>Usuário:</b> DevExample</p></li>
-                <li><p className='user-message'><b>Mensagem:</b> Que top, parabéns! </p></li>
+                <li><p className='user-name'><b>Usuário:</b> {initialPost.username} | {initialPost.email}</p></li>
+                <li><p className='user-message'><b>Mensagem:</b>{initialPost.comment}</p></li>
             </ul>
         </div>
     )
