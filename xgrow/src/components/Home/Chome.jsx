8f99f74925750f57f6f9e70bc6cc4e01c9841c91
Alessandre <156126428+alessandreviaro@users.noreply.github.com>
2024-04-14 19:53:40 -0300

    adjuting the home page style

--- xgrow/src/components/Home/Chome.jsx
+++ xgrow/src/components/Home/Chome.jsx
@@ -19,8 +19,10 @@ const Chome = ({ items }) => {
 
 
   const initialPost = items.find(item => item.id === '1');
+  const secondPost = items.find(item => item.id === '2');
+  const thirdPost = items.find(item => item.id === '3');
+  const fourthPost = items.find(item => item.id === '4');
 
-  const listPosts = items.filter(item => parseInt(item.id) >= 2 && parseInt(item.id) <= 4);
 
   return (
     <div>
@@ -31,7 +33,7 @@ const Chome = ({ items }) => {
             <h1 className="blog-title">{initialPost.title}</h1>
             <h4 className="blog-description">Aqui você ficará bem informado como nosso blog</h4>
             <Link to="/posts">
-            <img className="image-post" src={initialPost.image_url} alt="img-api"></img>
+            <img className="image-post" src="https://source.unsplash.com/1192x357/?random" alt="img-api"></img>
             <p className="post-description">{truncateString(initialPost.body, MAX_WORDS)}</p>
             <p className="data-minute">{initialPost.time_read} Min • {initialPost.created_at}</p> 
             </Link>
@@ -41,20 +43,53 @@ const Chome = ({ items }) => {
 
       <div className="list-post">
         <div className="post_feed">
-          <ul>
-            {listPosts.map((item) => (
-              <li key={item.id}>
-                <article className={`article-${item.id}`}>
-                  <img className="image-posts" src={item.image_url} alt="Random" />
+    
+            {secondPost && (
+              <div key={secondPost.id}>
+                <img className="image-post" src={secondPost.image_url} alt="Random" />
+                <article className={`article-${secondPost.id}`}>
                   <div className="feed-post-text">
-                    <h3 className="headline">{item.title}</h3>
-                    <p className="hd-description">{truncateString(item.body, MAX_WORDS)}</p>
-                    <p className="day-data">{item.time_read} Min • {item.created_at}</p>
+                    <h3 className="headline">{secondPost.title}</h3>
+                    <p className="hd-description">{truncateString(secondPost.body, MAX_WORDS)}</p>
+                    <p className="day-data">{secondPost.time_read} Min • {secondPost.created_at}</p>
                   </div>
                 </article>
-              </li>
-            ))}
-          </ul>
+              </div>
+            )}
+        </div>
+      </div>
+      <div className="list-post">
+        <div className="post_feed">
+          
+            {secondPost && (
+              <div key={thirdPost.id}>
+                <article className={`article-${thirdPost.id}`}>
+                  <div className="feed-post-text">
+                    <img className="image-post" src={thirdPost.image_url} alt="Random" />
+                    <h3 className="headline">{thirdPost.title}</h3>
+                    <p className="hd-description">{truncateString(thirdPost.body, MAX_WORDS)}</p>
+                    <p className="day-data">{thirdPost.time_read} Min • {thirdPost.created_at}</p>
+                  </div>
+                </article>
+              </div>
+            )}
+          
+        </div>
+      </div>
+      <div className="list-post">
+        <div className="post_feed">
+          {secondPost && (
+              <div key={fourthPost.id}>
+                <article className={`article-${fourthPost.id}`}>
+                  <div className="feed-post-text">
+                    <img className="image-post" src={fourthPost.image_url} alt="Random" />
+                    <h3 className="headline">{fourthPost.title}</h3>
+                    <p className="hd-description">{truncateString(fourthPost.body, MAX_WORDS)}</p>
+                    <p className="day-data">{fourthPost.time_read} Min • {fourthPost.created_at}</p>
+                  </div>
+                </article>
+              </div>
+            )}
         </div>
       </div>
     </div>
